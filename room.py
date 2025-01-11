3b366a27a3566231cefdbd71c79ff19b222feda2
Par Tojkander <par.tojkander@gmail.com>
2025-01-11 11:47:13 +0100

    Fix bug where room was bottom wall was not drawn

--- room.py
+++ room.py
@@ -17,19 +17,19 @@ class Room:
 
     def draw(self, screen : window) -> None:
         screen.addstr(self.y_offset, self.x_offset, self.wall * self.width)
-        for i in range(self.y_offset+1, self.y_offset + self.height-2 ):
+        for i in range(self.y_offset+1, self.y_offset + self.height-1):
             screen.addstr(i, self.x_offset, self.wall + self.floor * (self.width-2) + self.wall)
-        screen.addstr(self.y_offset + self.height-2, self.x_offset, self.wall * self.width)
+        screen.addstr(self.y_offset + self.height-1, self.x_offset, self.wall * self.width)
 
     def is_inside_room(self, x, y) -> bool:
         return (self.x_offset <= x < self.x_offset + self.width) and \
-               (self.y_offset <= y < self.y_offset + self.height-1)
+               (self.y_offset <= y < self.y_offset + self.height)
 
     def can_move_to(self, x, y) -> bool:
         if not self.is_inside_room(x, y):
             # We don't know that the player _cannot_ move there, so return true
             return True
-        if y == self.y_offset or y == self.y_offset + self.height-2:  # In top or bottom wall
+        if y == self.y_offset or y == self.y_offset + self.height-1:  # In top or bottom wall
             return False
         if x == self.x_offset or x == self.x_offset + self.width-1:   # In left or right wall
             return False
